8058bdfe5e16b9cd1e044094574f84762d16a32a
GVignesh-SD <gvignesh380980@gmail.com>
2024-03-21 02:50:41 +0530

    added the admin dashboard and completed the orders and customers page

---
+++ components/custom ui/SalesChart.tsx
@@ -0,0 +1,32 @@
+"use client";
+import React from "react";
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+} from "recharts";
+
+const SalesChart = ({ data }: { data: any[] }) => {
+  return (
+    <ResponsiveContainer width="100%" height={300}>
+      <LineChart
+        className="w-full h-full"
+        data={data}
+        margin={{ top: 5, right: 20 }}
+      >
+        <Line type={"monotone"} dataKey={"sales"} stroke="#8884d8" />
+        <CartesianGrid stroke="#ccc" strokeDasharray={"5 5"} />
+        <XAxis dataKey={"name"} />
+        <YAxis />
+        <Tooltip />
+      </LineChart>
+    </ResponsiveContainer>
+  );
+};
+
+export default SalesChart;
---
+++ lib/actions/action.ts
@@ -0,0 +1,48 @@
+import Customer from "../models/Customer";
+import Order from "../models/Order";
+import { connectToDB } from "../mongoDB";
+
+export const getTotalSales = async () => {
+  await connectToDB();
+  const orders = await Order.find();
+  const totalOrders = orders.length;
+  const totalRevenue = orders.reduce(
+    (acc, order) => acc + order.totalAmount,
+    0
+  );
+  return { totalOrders, totalRevenue };
+};
+
+export const getTotalCustomer = async () => {
+  await connectToDB();
+
+  const customer = await Customer.find();
+
+  const totalCustomer = customer.length;
+
+  return totalCustomer;
+};
+
+export const getSalesPerMonth = async () => {
+  await connectToDB();
+  const orders = await Order.find();
+
+  const salesPerMonth = orders.reduce((acc, order) => {
+    const monthIndex = new Date(order.createdAt).getMonth();
+    acc[monthIndex] = (acc[monthIndex] || 0) + order.totalAmount;
+    return acc;
+  }, {}) as any;
+
+  const graphData = Array.from({ length: 12 }, (_, i) => {
+    const month = new Intl.DateTimeFormat("en-us", { month: "short" }).format(
+      new Date(0, i)
+    );
+
+    return {
+      name: month,
+      sales: salesPerMonth[i] || 0,
+    };
+  });
+
+  return graphData;
+};
