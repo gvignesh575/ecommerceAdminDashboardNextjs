6bdae49560ab7d9c8833d9a5ae6e806d90c59167
GVignesh-SD <gvignesh380980@gmail.com>
2024-03-22 00:49:42 +0530

    test 1

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -10,53 +10,54 @@ import { UserButton } from "@clerk/nextjs";
 import { CircleDollarSign, ShoppingBag } from "lucide-react";
 
 export default async function Home() {
-  const totalRevenue = await getTotalSales().then((data) => data.totalRevenue);
-  const totalOrders = await getTotalSales().then((data) => data.totalOrders);
-  const totalCustomers = await getTotalCustomer();
+  // const totalRevenue = await getTotalSales().then((data) => data.totalRevenue);
+  // const totalOrders = await getTotalSales().then((data) => data.totalOrders);
+  // const totalCustomers = await getTotalCustomer();
 
-  const graphData = await getSalesPerMonth();
+  // const graphData = await getSalesPerMonth();
 
   return (
-    <div className="px-8 py-10">
-      <p className="text-heading2-bold">Dashboard</p>
-      <Separator className="bg-grey-1 my-5" />
-      <div className="grid gird-cols-2 md:grid-cols-3 gap-10">
-        <Card>
-          <CardHeader className="flex flex-row justify-between items-center">
-            <CardTitle>Total Revenue</CardTitle>
-            <CircleDollarSign className="max-sm:hidden" />
-          </CardHeader>
-          <CardContent>
-            <p className="text-body-bold">${totalRevenue}</p>
-          </CardContent>
-        </Card>
-        <Card>
-          <CardHeader className="flex flex-row justify-between items-center">
-            <CardTitle>Total Orders</CardTitle>
-            <ShoppingBag className="max-sm:hidden" />
-          </CardHeader>
-          <CardContent>
-            <p className="text-body-bold">{totalOrders}</p>
-          </CardContent>
-        </Card>
-        <Card>
-          <CardHeader className="flex flex-row justify-between items-center">
-            <CardTitle>Total Customer</CardTitle>
-            <CircleDollarSign className="max-sm:hidden" />
-          </CardHeader>
-          <CardContent>
-            <p className="text-body-bold">{totalCustomers}</p>
-          </CardContent>
-        </Card>
-      </div>
-      <Card className="mt-10">
-        <CardHeader>
-          <CardTitle>Sales Chart ($)</CardTitle>
-        </CardHeader>
-        <CardContent>
-          <SalesChart data={graphData} />
-        </CardContent>
-      </Card>
-    </div>
+    // <div className="px-8 py-10">
+    //   <p className="text-heading2-bold">Dashboard</p>
+    //   <Separator className="bg-grey-1 my-5" />
+    //   <div className="grid gird-cols-2 md:grid-cols-3 gap-10">
+    //     <Card>
+    //       <CardHeader className="flex flex-row justify-between items-center">
+    //         <CardTitle>Total Revenue</CardTitle>
+    //         <CircleDollarSign className="max-sm:hidden" />
+    //       </CardHeader>
+    //       <CardContent>
+    //         <p className="text-body-bold">${totalRevenue}</p>
+    //       </CardContent>
+    //     </Card>
+    //     <Card>
+    //       <CardHeader className="flex flex-row justify-between items-center">
+    //         <CardTitle>Total Orders</CardTitle>
+    //         <ShoppingBag className="max-sm:hidden" />
+    //       </CardHeader>
+    //       <CardContent>
+    //         <p className="text-body-bold">{totalOrders}</p>
+    //       </CardContent>
+    //     </Card>
+    //     <Card>
+    //       <CardHeader className="flex flex-row justify-between items-center">
+    //         <CardTitle>Total Customer</CardTitle>
+    //         <CircleDollarSign className="max-sm:hidden" />
+    //       </CardHeader>
+    //       <CardContent>
+    //         <p className="text-body-bold">{totalCustomers}</p>
+    //       </CardContent>
+    //     </Card>
+    //   </div>
+    //   <Card className="mt-10">
+    //     <CardHeader>
+    //       <CardTitle>Sales Chart ($)</CardTitle>
+    //     </CardHeader>
+    //     <CardContent>
+    //       <SalesChart data={graphData} />
+    //     </CardContent>
+    //   </Card>
+    // </div>
+    <div>Hello world</div>
   );
 }
