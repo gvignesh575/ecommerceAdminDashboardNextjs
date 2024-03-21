96f8fa4fc7bc9a4e9243b6cec282616b8667aa96
GVignesh-SD <gvignesh380980@gmail.com>
2024-03-22 02:22:54 +0530

    add the admin dashboard url

--- app/(dashboard)/orders/[orderId]/page.tsx
+++ app/(dashboard)/orders/[orderId]/page.tsx
@@ -3,7 +3,9 @@ import { columns } from "@/components/orderItems/OrderItemsColums";
 import { Separator } from "@/components/ui/separator";
 
 const OrderDetails = async ({ params }: { params: { orderId: string } }) => {
-  const res = await fetch(`http://localhost:3000/api/orders/${params.orderId}`);
+  const res = await fetch(
+    `${process.env.ADMIN_DASHBOARD_URL}/api/orders/${params.orderId}`
+  );
 
   const { orderDetails, customer } = await res.json();
 
