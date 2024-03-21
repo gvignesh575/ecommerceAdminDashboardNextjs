bc0a20b4bf39a8a321e110bb833cd6e0f2c823f7
GVignesh-SD <gvignesh380980@gmail.com>
2024-03-22 00:04:46 +0530

    added the loader

--- app/(dashboard)/collections/page.tsx
+++ app/(dashboard)/collections/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { columns } from "@/components/collections/CollectionColumns";
 import { DataTable } from "@/components/custom ui/DataTable";
+import Loader from "@/components/custom ui/Loader";
 import { Button } from "@/components/ui/button";
 import { Separator } from "@/components/ui/separator";
 import { Plus } from "lucide-react";
@@ -34,22 +35,28 @@ const Collections = () => {
   }, []);
 
   return (
-    <div className="px-10 py-5">
-      <div className="flex items-center justify-between">
-        <p className="text-heading2-bold">Collections</p>
-        <Button
-          className="bg-blue-1 text-white"
-          onClick={() => {
-            router.push("/collections/new");
-          }}
-        >
-          <Plus className="h-4 w-4 mr-2" />
-          Create Collection
-        </Button>
-      </div>
-      <Separator className="bg-grey-1 my-4" />
-      <DataTable columns={columns} data={collections} searchKey={"title"} />
-    </div>
+    <>
+      {loading ? (
+        <Loader />
+      ) : (
+        <div className="px-10 py-5">
+          <div className="flex items-center justify-between">
+            <p className="text-heading2-bold">Collections</p>
+            <Button
+              className="bg-blue-1 text-white"
+              onClick={() => {
+                router.push("/collections/new");
+              }}
+            >
+              <Plus className="h-4 w-4 mr-2" />
+              Create Collection
+            </Button>
+          </div>
+          <Separator className="bg-grey-1 my-4" />
+          <DataTable columns={columns} data={collections} searchKey={"title"} />
+        </div>
+      )}
+    </>
   );
 };
 
